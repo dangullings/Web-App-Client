5e24116bcb62acc70e9434b73314a32832eaedab
dangullings <dangullings@gmail.com>
2021-05-22 18:21:39 -0500

    Config

--- src/user_role/MyGroup.js
+++ src/user_role/MyGroup.js
@@ -279,7 +279,7 @@ class MyGroup extends Component {
 
   getMyPeepsList() {
     let promise;
-    promise = getMyPeeps(4); //this.state.currentUser.id
+    promise = getMyPeeps(this.state.currentUser.id);
     if (!promise) {
       return;
     }
